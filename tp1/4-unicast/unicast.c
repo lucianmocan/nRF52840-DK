22316c394446eb63c4130e1bbbb6d291c33b9cd3
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2025-10-01 22:54:19 +0200

    update unicast

--- tp1/4-unicast/unicast.c
+++ tp1/4-unicast/unicast.c
@@ -7,11 +7,11 @@
 
 #define BUFSIZE 256
 
-// TODO1 création structure message
+// create the message struct
 struct message {
     uint8_t seq_num;
-    char text[10000];
-};
+    char text[BUFSIZE];
+} __attribute__((packed)); // no padding so seq_num is always 1 byte
 
 
 /*---------------------------------------------------------------------------*/
@@ -19,68 +19,57 @@ PROCESS(unicast_process, "Unicast on Timeout");
 AUTOSTART_PROCESSES(&unicast_process);
 /*---------------------------------------------------------------------------*/
 
-// fonction de réception des messages
+// receive callback function
 void input_callback(const void *data, uint16_t len, const linkaddr_t *src,
                     const linkaddr_t *dst)
 {
 
-    // TODO6 afficher la source ainsi que les informations contenues dans le
-    // message reçu
+    // display the source and the message
     struct message *msg = (struct message *)data;
-    printf("Message reçu de ");
+    printf("Message received: ");
     for(int i = 0; i < LINKADDR_SIZE; i++) {
         printf("%02x", src->u8[i]);
         if(i < LINKADDR_SIZE - 1) printf(":");
     }
-    printf(" - Texte: %s, Séquence: %d\n", msg->text, msg->seq_num);
+    printf(" - Text: %s, Sequence: %d\n", msg->text, msg->seq_num);
 }
 
 PROCESS_THREAD(unicast_process, ev, data)
 {
 
     static struct etimer timer_timer;
-    static linkaddr_t dest_addr = {{0xf4, 0xce, 0x36, 0x5a, 0xb2, 0xbe, 0x02, 0x74}};
-    // static linkaddr_t dest_addr = {{0xf4, 0xce, 0x36, 0x0e, 0xda, 0x64, 0xa5, 0x87}};
+    static linkaddr_t dest_addr = {{0xf4, 0xce, 0x36, 0x1d, 0xcc, 0x8f, 0xd3, 0x96}};
 
     PROCESS_BEGIN();
 
     etimer_set(&timer_timer, 5 * CLOCK_SECOND);
 
 
-    // initialiser le callback de réception
+    // initialize the receive callback
     nullnet_set_input_callback(input_callback);
 
 
-    // TODO2 créer le message à envoyer
-    static struct message msg;
+    // create the message to send
+    static struct message msg = {0};
     msg.seq_num = 0;
     strcpy(msg.text, "so, WHO IS THEO!!");
 
 
     while (1) {
 
-        // TODO3 attendre un événement de type "relâchement de bouton"
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_timer));
 
-        // TODO4 vérifier que l'événement est déclenché par le bouton 1 (par
-        // ex.)
-        // button_hal_button_t *btn = (button_hal_button_t *)data;
-        // if (btn->unique_id == 0) {
-
-        // TODO5 positionnez les variables associées à la couche nullnet
         nullnet_buf = (uint8_t *)&msg;
-        nullnet_len = strlen(msg.text) + 1 + sizeof(msg.seq_num);
+        nullnet_len = 1 + strlen(msg.text) + 1; // seq num + text length + null terminator
 
-        // envoi du message
+        // send message to destination
         NETSTACK_NETWORK.output(&dest_addr);
         printf("Message unicast envoyé vers %02x:%02x : %s (%d)\n", 
             dest_addr.u8[0], dest_addr.u8[1], msg.text, msg.seq_num);
 
         msg.seq_num++;
-        // }
 
         etimer_reset(&timer_timer);
-
     }
 
     PROCESS_END();
